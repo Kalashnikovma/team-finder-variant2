2e58308b99a6cee54274dc457f71a56c54a48314
Kalashnikovma <mihail.kalasnikov321@gmail.com>
2026-06-10 20:45:14 +0300

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -1,18 +1,11 @@
 from django.urls import path
-
 from . import views
 
-app_name = "projects"
-
 urlpatterns = [
-    path("", views.project_list_view, name="project_list"),
-    path("create-project/", views.create_project_view, name="create_project"),
-    path("<int:project_id>/edit/", views.edit_project_view, name="edit"),
-    path("<int:project_id>/complete/", views.complete_project_view, name="complete"),
-    path(
-        "<int:project_id>/toggle-participate/",
-        views.toggle_participate,
-        name="toggle_participate",
-    ),
-    path("<int:project_id>/", views.project_detail, name="detail"),
+    path('list/', views.project_list, name='project_list'),
+    path('create-project/', views.create_project, name='create_project'),
+    path('<int:pk>/', views.project_detail, name='project_detail'),
+    path('<int:pk>/edit/', views.edit_project, name='edit_project'),
+    path('<int:pk>/complete/', views.complete_project, name='complete_project'),
+    path('<int:pk>/toggle-participate/', views.toggle_participate, name='toggle_participate'),
 ]
