38d4831bd1d9c5b8b7c14b2c79e8e252d8fc3546
Kalashnikovma <mihail.kalasnikov321@gmail.com>
2026-06-10 20:48:26 +0300

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,64 +1,21 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
+from django.contrib.auth.admin import UserAdmin
 from .models import User, Skill, UserSkill
 
 
 @admin.register(User)
-class UserAdmin(BaseUserAdmin):
-    list_display = ("username", "email", "name", "surname", "is_staff", "date_joined")
-    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
-
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'name', 'surname', 'is_staff')
+    ordering = ('email',)
     fieldsets = (
-        (None, {"fields": ("username", "email", "password")}),
-        (
-            "Личная информация",
-            {"fields": ("name", "surname", "about", "phone", "github_url", "avatar")},
-        ),
-        (
-            "Права доступа",
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
-                )
-            },
-        ),
-        ("Важные даты", {"fields": ("last_login", "date_joined")}),
+        (None, {'fields': ('email', 'password')}),
+        ('Персональная информация', {'fields': ('name', 'surname', 'avatar', 'phone', 'github_url', 'about')}),
+        ('Права доступа', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
     )
-
     add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": (
-                    "username",
-                    "email",
-                    "name",
-                    "surname",
-                    "password1",
-                    "password2",
-                ),
-            },
-        ),
+        (None, {'fields': ('email', 'name', 'surname', 'password1', 'password2')}),
     )
 
-    search_fields = ("username", "email", "name", "surname")
-    ordering = ("-date_joined",)
 
-
-@admin.register(Skill)
-class SkillAdmin(admin.ModelAdmin):
-    list_display = ("name",)
-    search_fields = ("name",)
-
-
-@admin.register(UserSkill)
-class UserSkillAdmin(admin.ModelAdmin):
-    list_display = ("user", "skill", "created_at")
-    list_filter = ("skill",)
-    search_fields = ("user__username", "user__email", "skill__name")
+admin.site.register(Skill)
+admin.site.register(UserSkill)
